4b48e9b15784e6327169ca1d5f8d587def360008
ChalitaK24 <schalitakel@gmail.com>
2024-12-05 15:14:44 -0800

    task crr

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -2,7 +2,8 @@
 
 /**
  * binary_tree_node - inserts a node as the left-child of another node
- *
+ * @parent: Pointer to the parent node of n_node
+ * @value: The value to put in n_node
  *
  * Return: n_node 
  */
@@ -12,7 +13,7 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	binary_tree_t *n_node = malloc(siteof(binary_tree_t));
 	n_node-> = value;
 	n_node->parent = parent;
-	n_node->left;
+ 	n_node->left = NULL;
 
 	return (n_node);
 }
