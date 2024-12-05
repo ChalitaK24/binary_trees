ace09f4689569cdf0fdecb2a981add8033fb904f
ChalitaK24 <schalitakel@gmail.com>
2024-12-05 15:45:03 -0800

    crrt

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -6,18 +6,19 @@
  * @parent: Pointer to the parent node of n_node
  * @value: The value to put in n_node
  *
- * Return: n_node 
+ * Return: n_node
  */
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *n_node = malloc(sizeof(binary_tree_t));
+
 	if (n_node == NULL)
 		return (NULL);
 
 	n_node->n = value;
 	n_node->parent = parent;
- 	n_node->left = NULL;
+	n_node->left = NULL;
 	n_node->right = NULL;
 
 	return (n_node);
