9f767df878d221d74d5733978168eb0f9c3b86a2
ChalitaK24 <schalitakel@gmail.com>
2024-12-05 15:10:42 -0800

    task cr

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,11 +1,18 @@
 #include "binary_trees.h"
 
 /**
+ * binary_tree_node - inserts a node as the left-child of another node
  *
  *
- *
- * Return: 
+ * Return: n_node 
  */
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *n_node = malloc(siteof(binary_tree_t));
+	n_node-> = value;
+	n_node->parent = parent;
+	n_node->left;
 
+	return (n_node);
+}
