02c532e7d10a1bd9893db13f4ae0fc2badbbfc7c
ChalitaK24 <schalitakel@gmail.com>
2024-12-05 18:18:53 -0800

    vrr

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -3,17 +3,17 @@
 /**
  * binary_tree_is_root - checks if a given node is a root
  *
- *
- * Return:
+ * @node: Pointer to the node to be checked
+ * Return: 1 if root or 0
  */
 
 int binary_tree_is_root(const binary_tree_t *node)
 {
 	if (node == NULL)
-		return (1);
+		return (0);
 
 	if (node->parent == NULL)
-		return(1);
-	
+		return (1);
+
 	return (0);
 }
