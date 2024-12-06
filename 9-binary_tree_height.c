ebde3882f3c876e075c72f999a60a39ab5c4b54c
ChalitaK24 <schalitakel@gmail.com>
2024-12-06 14:13:33 -0800

    task cr

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,10 +1,23 @@
 # include "binary_trees.h"
 
 /**
+ * binary_tree_height - measures the height of a binary tree
+ * @tree:  Pointer to the root node of the tree
  *
- *
- *
- * Return:
+ * Return: Height or 0 if NULL
  */
 
 size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t l_height;
+	size_t r_height;
+
+	if (tree = NULL)
+		return (0);
+
+	l_height =
+
+	r_height = 
+
+
+}
