e189ff17cb72d08390940d77b9ac0d06a8bc0b25
ChalitaK24 <schalitakel@gmail.com>
2024-12-06 12:43:02 -0800

    crr

---
+++ 6-binary_tree_preorder.c
@@ -0,0 +1,17 @@
+# include "binary_trees.h"
+
+/**
+ * binary_tree_preorder - goes through a binary tree using pre-order traversal
+ * @tree: Pointer to node to traverse
+ * @func: Pointer to the func call to each node
+ *
+ * Return: void
+ */
+
+void binary_treie_preorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree == NULL || func == NULL)
+		return;
+
+	func(tree->n);
+}
