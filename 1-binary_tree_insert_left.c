eef1abf040b514319dcf051d061f04d3330f7366
ChalitaK24 <schalitakel@gmail.com>
2024-12-05 16:39:02 -0800

    crru

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -12,6 +12,9 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *n_node;
 
+	if (parent == NULL)
+		return (NULL);
+
 	n_node = malloc(sizeof(binary_tree_t));
 
 	if (n_node == NULL)
@@ -22,7 +25,10 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	n_node->left = parent->left;
 	n_node->right = NULL;
 
+	if (parent->left != NULL)
+		 parent->left = new_node;
 
 	parent->left = n_node;
 
 	return( n_node);
+}
