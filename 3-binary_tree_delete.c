3dbb50ed681925911f720b6de6e41d502e174320
ChalitaK24 <schalitakel@gmail.com>
2024-12-05 17:13:09 -0800

    tr3

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_delete - deletes an entire binary tree
+ *
+ *
+ * @root_tree: Pointer to the root node
+ */
+
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (
+		return ();
+
+}
