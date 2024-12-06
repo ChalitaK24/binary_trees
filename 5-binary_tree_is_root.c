f23d538163708eec63adcad08121820a604fa4c6
ChalitaK24 <schalitakel@gmail.com>
2024-12-05 18:12:14 -0800

    ctask

--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -9,11 +9,11 @@
 
 int binary_tree_is_root(const binary_tree_t *node)
 {
-	if (node == NULL)n
+	if (node == NULL)
 		return (1);
 
 	if (node->parent == NULL)
-		return(1)
+		return(1);
 	
 	return (0);
 }
