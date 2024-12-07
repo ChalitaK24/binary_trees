730ed2b9a74049d9f6c84a0833fe43ec528727da
ChalitaK24 <schalitakel@gmail.com>
2024-12-06 18:41:37 -0800

    tak

---
+++ 15-binary_tree_is_full.c
@@ -0,0 +1,14 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_full - checks if a binary tree is full
+ *
+ * @tree: pointer to the root node of the tree to check
+ *
+ * Return: 
+ */
+
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+
+}
