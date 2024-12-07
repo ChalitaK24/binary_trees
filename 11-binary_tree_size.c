3339efa4cf3a1cbe4b0511f7baaaeb2aeefa6eb7
ChalitaK24 <schalitakel@gmail.com>
2024-12-06 18:27:34 -0800

    t11

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -10,13 +10,9 @@
 
 
 size_t binary_tree_size(const binary_tree_t *tree)
-
 {
 	if (tree == NULL)
 		return (0);
 
-	size_t l_size = binary_tree_size(tree->left);
-	size_t r_size = binary_tree_size(tree->right);
-
-	return (l_size + r_size + 1);
+	return (binary_tree_size(tree->left) + binary_tree_size(tree->right) + 1);
 }
