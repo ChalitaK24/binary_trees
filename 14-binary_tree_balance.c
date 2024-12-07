09b26410e4ce7efcc0b19319dafa4765e999a1a3
ChalitaK24 <schalitakel@gmail.com>
2024-12-06 18:34:27 -0800

    13cr

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,10 +1,16 @@
 #include "binary_trees.h"
 
 /**
+ * binary_tree_balance -
  *
  *
- *
- * Returm:
+ * Returm: Balance factor 0 if NULL
  */
 
 int binary_tree_balance(const binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return (0);
+
+	return (l_height - r_height);
+}
